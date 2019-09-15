3d8ec9705c363ca770b3b7450908de4c4dc696db
matthew-haines <m.haines1000@gmail.com>
2019-09-15 00:42:46 -0400

    combined

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask import jsonify
-from views import views
-from resources import resources 
+from flask_restful import Resource, reqparse
 from flask_cors import CORS 
-from views import views
 import threading
 from ingest import TDApi
+import json
+from flask import Blueprint
+from flask import jsonify
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,7 +16,29 @@ tdapi = TDApi()
 
 uid = None
 budget = 0.0
-alerts = []
+alerts = {}
+
+default_page = Blueprint('default_page', __name__)
+
+@default_page.route('/')
+def index():
+    return jsonify({'Status':'This site is working'})
+
+
+class UserData(Resource):
+    def get(self, user_id, user_budget):
+        uid = user_id
+        budget = user_budget
+        return {
+            'Status' : 'Welcome to the club'
+        }
+
+
+class AlertSystem(Resource):
+    def get(self):
+        global alerts
+        return json.dumps(alerts)
+
 
 def update():
     # start this by calling it once we get the UID
@@ -24,21 +47,21 @@ def update():
     current_spent = tdapi.total_monthly_spending(transactions['09'])
     predicted_spent = tdapi.predicted_monthly_spending(transactions['09'])
     if current_spent > budget:
-        alerts.append({'OverBudget': {'Amount': current_spent - budget}})
-    elif predicted_spent > budget:
-        alerts.append({'PredictedOverBudget': {'Amount': predicted_spent - budget}})
+        alerts['OverBudget'] = {'Amount': current_spent - budget}
 
-    
+    elif predicted_spent > budget:
+        alerts['PredictedOverBudget'] = {'Amount': predicted_spent - budget}
 
 
 CORS(app)
 
 #API HOME PAGE
 
-app.register_blueprint(views.default_page)
+app.register_blueprint(default_page)
 
 #User resources
-api.add_resource(resources.UserData, '/user/<int:user_id>')
+api.add_resource(UserData, '/user/<int:user_id>')
+api.add_resource(AlertSystem, '/alerts/')
 
 if __name__ == '__main__':
     app.run(debug=True)
