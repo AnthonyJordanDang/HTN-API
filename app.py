f1d71900a015532cb09dc6c087c5d0ed7a1a6d3c
matthew-haines <m.haines1000@gmail.com>
2019-09-14 21:28:40 -0400

    update_loop

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ from flask_restful import Api
 from flask import jsonify
 from views import views
 from resources import resources 
-from flask_cors import CORS
+from flask_cors import CORS 
 from views import views
+import threading
+from ingest import TDApi
 
 app = Flask(__name__)
 api = Api(app)
 
+tdapi = TDApi()
+
 uid = None
 transactions = None
+
+def update_transactions():
+    # start this by calling it once we get the UID
+    threading.Timer(15, update_transactions).start()
+    transactions = tdapi.get_past_transactions(uid)
 
 CORS(app)
 
