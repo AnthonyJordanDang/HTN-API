1df809977bbfb8705792aa332d53e30f8c3472a0
matthew-haines <m.haines1000@gmail.com>
2019-09-14 21:15:07 -0400

    ingest

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask import jsonify
 from views import views
@@ -9,9 +9,13 @@ from views import views
 app = Flask(__name__)
 api = Api(app)
 
+uid = None
+transactions = None
+
 CORS(app)
 
 #API HOME PAGE
+
 app.register_blueprint(views.default_page)
 
 #User resources
